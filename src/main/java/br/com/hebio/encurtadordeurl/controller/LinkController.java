a0ba6143ff4b65a939083638d648e16ca0163762
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2023-06-04 13:40:20 -0300

    Refatoracao das Respostas HTTP

--- src/main/java/br/com/hebio/encurtadordeurl/controller/LinkController.java
+++ src/main/java/br/com/hebio/encurtadordeurl/controller/LinkController.java
@@ -4,8 +4,12 @@ import br.com.hebio.encurtadordeurl.model.Link;
 import br.com.hebio.encurtadordeurl.repository.LinkRepository;
 import br.com.hebio.encurtadordeurl.service.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -19,31 +23,46 @@ public class LinkController {
     private LinkService linkService;
 
     @GetMapping()
-    public Iterable<Link> listLinks() {
+    public ResponseEntity<Iterable<Link>> listLinks() {
         Iterable<Link> links = linkRepository.findAll();
-        return links;
+        return ResponseEntity.ok().body(links);
     }
 
     @PostMapping()
-    public void generateCode(@RequestBody Link link) {
+    public ResponseEntity<Void> generateCode(@RequestBody Link link) {
         link.setCode(linkService.generateCode());
-        linkRepository.save(link);
+        link = linkRepository.save(link);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(link.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping("/{id}")
-    public Optional<Link> searchLink(@PathVariable("id") Long id) {
-        return linkRepository.findById(id);
+    public ResponseEntity<?> searchLink(@PathVariable("id") Long id) {
+        Optional<Link> link = linkRepository.findById(id);
+        if (!link.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(link);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteLink(@PathVariable("id") Long id) {
-        linkRepository.deleteById(id);
+    public ResponseEntity<Void> deleteLink(@PathVariable("id") Long id) {
+        try {
+            linkRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/{id}")
-    public void updateLink(@RequestBody Link link, @PathVariable("id") Long id) {
+    public ResponseEntity<Void> updateLink(@RequestBody Link link, @PathVariable("id") Long id) {
         Optional<Link> linkAux = linkRepository.findById(id);
         linkAux.get().setUrl(link.getUrl());
         linkRepository.save(linkAux.get());
+        return ResponseEntity.noContent().build();
     }
 }
